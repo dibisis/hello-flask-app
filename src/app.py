179246bf7a7825cb8682723742e7e0ec64327ecb
dibisis <dibisis@gmail.com>
2020-08-21 10:55:38 +0900

    init commit

---
+++ src/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
+
+
+@app.route('/hello/<username>')  # api for vars
+def hello_user(username):
+    return 'Hello you, %s!\n' % username
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')  # open for everyone
